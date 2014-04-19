449cf3c8afbf1a031068f89d3ffd133328b5f342
Deborah <deborah@arcoiris-ThinkPad-T400.(none)>
2014-04-19 06:50:53 -0400

    Add users and foundation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
