6f30c3db7d36453742b52c77322b87c5c8c1f78b
Deborah <deborah@arcoiris-ThinkPad-T400.(none)>
2014-04-19 07:48:19 -0400

    Configure devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :opportunities
-  has_many :notification, through: :listings
+  has_many :notifications, through: :listings
 end
